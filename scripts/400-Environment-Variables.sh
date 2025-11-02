7fcf498c0e38c07f4f8065f396b3685ce7796d17
Brad Hutchings <brad@componentx.com>
2025-11-02 09:40:41 -0800

    Update and rename 300-Environment-Variables.sh to 400-Environment-Variables.sh

--- scripts/400-Environment-Variables.sh
+++ scripts/400-Environment-Variables.sh
@@ -1,12 +1,12 @@
 # Environment Variables corresponding to the "300. Build Mmojo Server" section. 
 
 cd $HOME
-export MODELS_DIR="$HOME/201-MODELS"
-export BUILD_COSMOPOLITAN_DIR="$HOME/202-BUILD-cosmopolitan"
+export MODELS_DIR="$HOME/301-MODELS"
+export BUILD_COSMOPOLITAN_DIR="$HOME/302-BUILD-cosmopolitan"
 export COSMOCC_DIR="$HOME/$BUILD_COSMOPOLITAN_DIR/cosmocc"
-export BUILD_LLAMAFILE_DIR="$HOME/203-BUILD-llamafile"
-export BUILD_OPENSSSL_DIR="$HOME/204-BUILD-openssl"
-export BUILD_LLAMA_CPP_DIR="$HOME/300-BUILD-lamma-cpp"
+export BUILD_LLAMAFILE_DIR="$HOME/303-BUILD-llamafile"
+export BUILD_OPENSSSL_DIR="$HOME/304-BUILD-openssl"
+export BUILD_LLAMA_CPP_DIR="$HOME/400-BUILD-lamma-cpp"
 export MMOJO_SERVER_DIR="$BUILD_LLAMA_CPP_DIR/mmojo-server"
 if [ -z "$SAVE_PATH" ]; then
   export SAVE_PATH=$PATH
