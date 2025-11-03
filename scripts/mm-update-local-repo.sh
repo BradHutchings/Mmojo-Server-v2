a1a4f19dbf769d9b6c61bdb0c120d1b2c4f37c1b
Brad Hutchings <brad@componentx.com>
2025-11-03 10:43:23 -0800

    Update mm-update-local-repo.sh

--- scripts/mm-update-local-repo.sh
+++ scripts/mm-update-local-repo.sh
@@ -5,8 +5,9 @@ cd $MMOJO_SERVER_DIR
 git pull
 cd $WD
 
-# Can I soft link these instead?
-cp -f $MMOJO_SERVER_SCRIPTS/mm-* $HOME/scripts/
-chmod a+x $HOME/scripts/mm-*
+for FILE in $MMOJO_SERVER_SCRIPTS/mm-*.sh; do
+  ln -sf $FILE $HOME/scripts
+  chmod a+x $HOME/scripts/mm-*.sh
+done
 
 printf "\n**********\n*\n* FINISHED: mm-update-local-repo.\n*\n**********\n\n"
