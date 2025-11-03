21b609c8640337c6731d436a165967abe2960711
Brad Hutchings <brad@componentx.com>
2025-11-03 06:38:50 -0800

    Update mm-work-in-progress-branch.sh

--- scripts/mm-work-in-progress-branch.sh
+++ scripts/mm-work-in-progress-branch.sh
@@ -4,6 +4,10 @@ if [ -d "$MMOJO_SERVER_DIR" ]; then
   cd $MMOJO_SERVER_DIR
   git checkout work-in-progress
   cd $HOME
+
+  # Can I soft link these instead?
+  cp -f $MMOJO_SERVER_SCRIPTS/mm-* $HOME/scripts/
+  chmod a+x $HOME/scripts/mm-*
 else
   echo "The $MMOJO_SERVER_DIR directory does not exist."
 fi
