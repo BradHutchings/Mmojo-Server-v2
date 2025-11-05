607f46174a9a5ba68971f4af8019c9b7c861333b
Brad Hutchings <brad@componentx.com>
2025-11-05 06:05:35 -0800

    Update 305-Clone-Build-llamafile.sh

--- scripts/305-Clone-Build-llamafile.sh
+++ scripts/305-Clone-Build-llamafile.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [ -d "$BUILD_LLAMAFILE_DIR" ]; then
+    rm -r -f $BUILD_LLAMAFILE_DIR
+fi
+
 git clone https://github.com/Mozilla-Ocho/llamafile $BUILD_LLAMAFILE_DIR
 cd $BUILD_LLAMAFILE_DIR
+
+# Build llamafile
 make -j8
 make install PREFIX=.
+
 cd $HOME
+
 printf "\n**********\n*\n* FINISHED: 305-Clone-Build-llamafile.sh.\n*\n**********\n\n"
