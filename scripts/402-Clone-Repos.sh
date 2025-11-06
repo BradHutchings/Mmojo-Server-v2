3ac9b7fb8fa9881a89d06057fecd8d88fd315039
Brad Hutchings <brad@componentx.com>
2025-11-06 12:42:37 -0800

    Update 402-Clone-Repos.sh

--- scripts/402-Clone-Repos.sh
+++ scripts/402-Clone-Repos.sh
@@ -11,13 +11,13 @@ git clone https://github.com/google/minja.git google-minja
 git clone https://github.com/yhirose/cpp-httplib.git cpp-httplib
 git clone https://github.com/mackron/miniaudio.git miniaudio
 git clone https://github.com/nothings/stb.git stb
-cp -r ~/$BUILD_COSMOPOLITAN_DIR/cosmocc .
-cp -r ~/$BUILD_OPENSSSL_DIR/openssl .
+cp -r $BUILD_COSMOPOLITAN_DIR/cosmocc .
+cp -r $BUILD_OPENSSSL_DIR/openssl .
 
 # Roll the llama.cpp repo back to last known working.
 # Should we track to releases on the repo? No ides.
-echo "\n--------------------------------------------------------------------------------"
-echo "TO DO: Roll back llama.cpp to a known safe point."
-echo "--------------------------------------------------------------------------------"
+printf "\n--------------------------------------------------------------------------------"
+printf "TO DO: Roll back llama.cpp to a known safe point."
+printf "--------------------------------------------------------------------------------"
 
 printf "\n**********\n*\n* FINISHED: 402-Clone-Repos.sh.\n*\n**********\n\n"
