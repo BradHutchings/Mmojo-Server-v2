20e9024450d95805b0d0997252baf022bf2dc433
Brad Hutchings <brad@componentx.com>
2025-11-06 21:27:23 -0800

    Update 403-Test-Command-Line.sh

--- scripts/403-Test-Command-Line.sh
+++ scripts/403-Test-Command-Line.sh
@@ -6,7 +6,7 @@ cd $TEST_DIR
 
 rm -f mmojo-server-args
 rm -r -f mmojo-server-support
-./$BUILD_TEST/bin/mmojo-server --model $MODELS_DIR/Google-Gemma-1B-Instruct-v3-q8_0.gguf \
+$BUILD_LLAMA_CPP_DIR/$BUILD_TEST/bin/mmojo-server --model $MODELS_DIR/Google-Gemma-1B-Instruct-v3-q8_0.gguf \
     --path completion-ui/ --default-ui-endpoint "chat" --host 0.0.0.0 --port 8080 --batch-size 64 \
     --threads-http 8 --ctx-size 0 --mlock
 
