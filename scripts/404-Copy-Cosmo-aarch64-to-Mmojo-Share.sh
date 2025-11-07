07eae2c6e7ad414c6522d3c3802531a092f04b6b
Brad Hutchings <brad@componentx.com>
2025-11-07 10:18:08 -0800

    Update 404-Copy-Cosmo-aarch64-to-Mmojo-Share.sh

--- scripts/404-Copy-Cosmo-aarch64-to-Mmojo-Share.sh
+++ scripts/404-Copy-Cosmo-aarch64-to-Mmojo-Share.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mm-mount-mmojo-share.sh
-sudo mkdir -p MMOJO_SHARE_BUILDS
-sudo mkdir -p MMOJO_SHARE_BUILDS_APE
+sudo mkdir -p $MMOJO_SHARE_BUILDS
+sudo mkdir -p $MMOJO_SHARE_BUILDS_APE
 sudo cp -f $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_AARCH64/bin/mmojo-server $MMOJO_SHARE_BUILDS_APE/mmojo-server-aarch64
 
 printf "\n**********\n*\n* FINISHED: 404-Copy-Cosmo-aarch64-to-Mmojo-Share.sh.\n*\n**********\n\n"
