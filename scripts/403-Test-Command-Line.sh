7e4f504d029389b31b90c3b9dd2db6fdde797034
Brad Hutchings <brad@componentx.com>
2025-11-06 20:52:36 -0800

    Create 403-Test-Command-Line.sh

---
+++ scripts/403-Test-Command-Line.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $BUILD_LLAMA_CPP_DIR
+
+rm -f mmojo-server-args
+rm -r -f mmojo-server-support
+./$BUILD_TEST/bin/mmojo-server --model $MODELS_DIR/Google-Gemma-1B-Instruct-v3-q8_0.gguf \
+    --path completion-ui/ --default-ui-endpoint "chat" --host 0.0.0.0 --port 8080 --batch-size 64 \
+    --threads-http 8 --ctx-size 0 --mlock
+
+cd $HOME
+
+printf "\n**********\n*\n* FINISHED: 403-Test-Command-Line.sh.\n*\n**********\n\n"
