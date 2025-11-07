353d2b500e71c61b6d8e71376a2208d3b6268285
Brad Hutchings <brad@componentx.com>
2025-11-07 08:32:07 -0800

    Update 404-Assemble-Cosmo-APE-Local.sh

--- scripts/404-Assemble-Cosmo-APE-Local.sh
+++ scripts/404-Assemble-Cosmo-APE-Local.sh
@@ -7,9 +7,9 @@ export PATH="$(pwd)/cosmocc/bin:$SAVE_PATH"
 apelink \
 	-l $BUILD_COSMOPOLITAN_DIR/o/x86_64/ape/ape.elf \
 	-l $BUILD_COSMOPOLITAN_DIR/o/aarch64/ape/ape.elf \
-	-o $BUILD_APE/mmojo-server-ape \
-    $BUILD_COSMO_X86_64/bin/mmojo-server \
-    $BUILD_COSMO_AARCH64/bin/mmojo-server
+	-o $BUILD_LLAMA_CPP_DIR/$BUILD_APE/mmojo-server-ape \
+    $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_X86_64/bin/mmojo-server \
+    $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_AARCH64/bin/mmojo-server
 export PATH=$SAVE_PATH
 
 cd $HOME
