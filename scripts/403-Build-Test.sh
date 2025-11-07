f2d2e0782146724687f628f0aad870ec1e2c1db9
Brad Hutchings <brad@componentx.com>
2025-11-06 20:52:58 -0800

    Update 403-Build-Test.sh

--- scripts/403-Build-Test.sh
+++ scripts/403-Build-Test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd $BUILD_LLAMA_CPP_DIR
+
 if [ "$BUILD_PROFILE" ]; then
     export CC="cc -pg "
     export CXX="c++ -pg "
@@ -8,5 +9,7 @@ fi
 
 cmake -B $BUILD_TEST -DBUILD_SHARED_LIBS=OFF -DLLAMA_CURL=OFF -DLLAMA_OPENSSL=ON
 cmake --build $BUILD_TEST --config Release
+
+cd $HOME
 
 printf "\n**********\n*\n* FINISHED: 403-Build-Test.sh.\n*\n**********\n\n"
