ac86ede402e7960018be83be966cc4f4d58bae65
Brad Hutchings <brad@componentx.com>
2025-11-07 22:36:17 -0800

    Update 303-Create-Models-Directory.sh

--- scripts/303-Create-Models-Directory.sh
+++ scripts/303-Create-Models-Directory.sh
@@ -2,8 +2,11 @@
 
 mkdir -p $MODELS_DIR
 cd $MODELS_DIR
+
 cat << EOF > $MODEL_MAP
 Google-Gemma-1B-Instruct-v3-q8_0.gguf mmojo-server-Google-Gemma-1B-Instruct-v3
 EOF
+
+cd $HOME
 
 printf "\n**********\n*\n* FINISHED: 303-Create-Models-Directory.sh.\n*\n**********\n\n"
