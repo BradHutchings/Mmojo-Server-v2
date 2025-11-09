393128eaeb166c2c04bf5b46c842e2beae3f2070
Brad Hutchings <brad@componentx.com>
2025-11-09 10:02:22 -0800

    Update 207-Set-up-Cross-Compile-aarch64-on-x86_64.sh

--- scripts/207-Set-up-Cross-Compile-aarch64-on-x86_64.sh
+++ scripts/207-Set-up-Cross-Compile-aarch64-on-x86_64.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+################################################################################
+# This script installs what's needed to cross compile aarch64 (arm64) on x86_64.
+#
+# See licensing note at end.
+################################################################################
+
 sudo dpkg --add-architecture arm64
 sudo cat << EOF > ubuntu-arm64.sources
 Types: deb
@@ -15,3 +21,15 @@ sudo apt install -y libssl-dev:arm64
 sudo apt install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu binutils-aarch64-linux-gnu
 
 printf "\n**********\n*\n* FINISHED: 207-Set-up-Cross-Compile-aarch64-on-x86_64.sh.\n*\n**********\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
