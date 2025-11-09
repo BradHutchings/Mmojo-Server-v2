e5a286b78bb9bae5df98f3698205a4db7e397d0e
Brad Hutchings <brad@componentx.com>
2025-11-09 10:09:48 -0800

    Update 307-Clone-Build-llamafile.sh

--- scripts/307-Clone-Build-llamafile.sh
+++ scripts/307-Clone-Build-llamafile.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+################################################################################
+# This script clones and builds the llamafile Github repository, then rolls it
+# back to a known release we know how to build. I used llamafile for the
+# zipalign tool, but have since absorbed zipalign into this repo. I thank
+# Justine Tunney, Mozilla Foundation, and all contributors for the inspiration
+# llamafile gave me to create the Mmojo Server project. -Brad 
+#
+# See licensing note at end.
+################################################################################
+
 if [ -d "$BUILD_LLAMAFILE_DIR" ]; then
     rm -r -f $BUILD_LLAMAFILE_DIR
 fi
@@ -16,3 +26,15 @@ make install PREFIX=.
 cd $HOME
 
 printf "\n**********\n*\n* FINISHED: 307-Clone-Build-llamafile.sh.\n*\n**********\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
