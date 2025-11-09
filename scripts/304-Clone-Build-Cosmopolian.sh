af2d4e18787353ad5df0cb7502b71051ae2a459f
Brad Hutchings <brad@componentx.com>
2025-11-09 09:07:54 -0800

    Update 304-Clone-Build-Cosmopolian.sh

--- scripts/304-Clone-Build-Cosmopolian.sh
+++ scripts/304-Clone-Build-Cosmopolian.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This script clones and builds the Cosmpolitan Github repository. We use it to build
+# Actual Portable Executables (APE). Thank you to Justine Tunney and team for 
+# the Cosmopolitan project!
+#
+# https://github.com/jart/cosmopolitan
+#
+# See licensing note at end.
+
 if [ -d "$BUILD_COSMOPOLITAN_DIR" ]; then
     rm -r -f $BUILD_COSMOPOLITAN_DIR
 fi
@@ -22,15 +30,15 @@ cd $HOME
 
 printf "\n**********\n*\n* FINISHED: 304-Clone-Build-Cosmopolian.sh.\n*\n**********\n\n"
 
-/*
-  This is an original script for the Mmojo Server repo. It is covered by
-  the MIT-style LICENSE.
-
-  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
-
-  Copyright (c) 2025 Brad Hutchings.
-  --
-  Brad Hutchings
-  brad@bradhutchings.com
-*/
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
 
