f45ab7b750c5e44d98291fa89b2b9dcb4cefbcef
Brad Hutchings <brad@componentx.com>
2025-11-08 21:29:02 -0800

    Update and rename 303-Download-Model-Map.sh to 302-Download-Model-Map.sh

--- scripts/302-Download-Model-Map.sh
+++ scripts/302-Download-Model-Map.sh
@@ -3,4 +3,4 @@
 MODEL_MAP_URL="https://huggingface.co/bradhutchings/Mmojo-Server/resolve/main/model-map.txt"
 wget $MODEL_MAP_URL --quiet -O $MODEL_MAP
 
-printf "\n**********\n*\n* FINISHED: 303-Download-Model-Map.sh.\n*\n**********\n\n"
+printf "\n**********\n*\n* FINISHED: 302-Download-Model-Map.sh.\n*\n**********\n\n"
