c4eafc9cb386c55b6c09f033a0b55f5050ba615c
Brad Hutchings <brad@componentx.com>
2025-11-08 17:16:28 -0800

    Update 404-Assemble-Cosmo-APE-Local.sh

--- scripts/404-Assemble-Cosmo-APE-Local.sh
+++ scripts/404-Assemble-Cosmo-APE-Local.sh
@@ -4,12 +4,21 @@ cd $BUILD_LLAMA_CPP_DIR
 
 mkdir -p $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_APE
 export PATH="$(pwd)/cosmocc/bin:$SAVE_PATH"
+
 apelink \
 	-l $BUILD_COSMOPOLITAN_DIR/o/x86_64/ape/ape.elf \
 	-l $BUILD_COSMOPOLITAN_DIR/o/aarch64/ape/ape.elf \
 	-o $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_APE/mmojo-server-ape \
     $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_X86_64/bin/mmojo-server \
     $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_AARCH64/bin/mmojo-server
+
+apelink \
+	-l $BUILD_COSMOPOLITAN_DIR/o/x86_64/ape/ape.elf \
+	-l $BUILD_COSMOPOLITAN_DIR/o/aarch64/ape/ape.elf \
+	-o $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_APE/mm-zipalign-ape \
+    $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_X86_64/bin/mm-zipalign \
+    $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_AARCH64/bin/mm-zipalign
+
 export PATH=$SAVE_PATH
 
 cd $HOME
