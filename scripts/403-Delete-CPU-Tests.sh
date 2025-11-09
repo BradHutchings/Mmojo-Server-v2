c033dade9aa7cf2d12308c8706781535cc9366c8
Brad Hutchings <brad@componentx.com>
2025-11-09 10:52:44 -0800

    Update and rename 403-Delete-Tests.sh to 403-Delete-CPU-Tests.sh

---
+++ scripts/403-Delete-CPU-Tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+################################################################################
+# This script deletes all of the $HOME/403-CPU-Test-* directories.
+#
+# See licensing note at end.
+################################################################################
+
+
+cd $HOME
+
+TEST_DIR="$HOME/403-CPU-Test-*"
+rm -r -f $TEST_DIR
+
+printf "\n**********\n*\n* FINISHED: 403-Delete-CPU-Tests.sh.\n*\n**********\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################`
--- scripts/403-Delete-Tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $HOME
-
-TEST_DIR="$HOME/403-Test-*"
-rm -r -f $TEST_DIR
-
-printf "\n**********\n*\n* FINISHED: 403-Delete-Tests.sh.\n*\n**********\n\n"
