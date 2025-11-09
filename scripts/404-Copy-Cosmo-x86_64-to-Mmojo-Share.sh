ad450ccad4d3912918cbab0d9e58bac81dcedfa9
Brad Hutchings <brad@componentx.com>
2025-11-08 22:18:46 -0800

    Update 404-Copy-Cosmo-x86_64-to-Mmojo-Share.sh

--- scripts/404-Copy-Cosmo-x86_64-to-Mmojo-Share.sh
+++ scripts/404-Copy-Cosmo-x86_64-to-Mmojo-Share.sh
@@ -4,5 +4,6 @@ mm-mount-mmojo-share.sh
 sudo mkdir -p $MMOJO_SHARE_BUILDS
 sudo mkdir -p $MMOJO_SHARE_BUILDS_APE
 sudo cp -f $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_X86_64/bin/mmojo-server $MMOJO_SHARE_BUILDS_APE/mmojo-server-x86_64
+sudo cp -f $BUILD_LLAMA_CPP_DIR/$BUILD_COSMO_X86_64/bin/mm-zipalign $MMOJO_SHARE_BUILDS_APE/mm-zipalign-x86_64
 
 printf "\n**********\n*\n* FINISHED: 404-Copy-Cosmo-x86_64-to-Mmojo-Share.sh.\n*\n**********\n\n"
