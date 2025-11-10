5c552b1be2113e0dd04f7fe426e8396f6d6412ab
Brad Hutchings <brad@componentx.com>
2025-11-09 19:10:12 -0800

    Update mm-environment-variables.sh

--- scripts/mm-environment-variables.sh
+++ scripts/mm-environment-variables.sh
@@ -52,6 +52,7 @@ fi
 
 export BUILD_LLAMA_CPP_DIR="$HOME/400-BUILD-lamma-cpp"
 export BUILD_CPU_DEBUG="build-cpu-debug"
+export BUILD_CPU_RELEASE="build-cpu-release"
 export BUILD_COSMO_X86_64="build-cosmo-x86_64"
 export BUILD_COSMO_AARCH64="build-cosmo-aarch64"
 export BUILD_COSMO_APE="build-cosmo-ape"
