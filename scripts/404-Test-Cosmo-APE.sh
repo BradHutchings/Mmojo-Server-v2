0606d0ac5e3d42e2a5f80e464b7a1f3edd5c88b5
Brad Hutchings <brad@componentx.com>
2025-11-11 08:26:27 -0800

    Update 404-Test-Cosmo-APE.sh

--- scripts/404-Test-Cosmo-APE.sh
+++ scripts/404-Test-Cosmo-APE.sh
@@ -6,7 +6,7 @@
 # See licensing note at end.
 ################################################################################
 
-TEST_DIR="$HOME/404-Test-Cosmo-ape"
+TEST_DIR="$TEST_LLAMA_CPP_DIR/$TEST_COSMO_APE"
 mkdir -p $TEST_DIR
 rm -r -f $TEST_DIR/*
 cd $TEST_DIR
