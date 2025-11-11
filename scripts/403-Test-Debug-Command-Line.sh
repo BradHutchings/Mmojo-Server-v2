d7cf65ed29f58d3b75ed13dd1e47c7b13876ea13
Brad Hutchings <brad@componentx.com>
2025-11-11 06:05:41 -0800

    Update and rename 403-Test-CPU-Debug-Command-Line.sh to 403-Test-Debug-Command-Line.sh

--- scripts/403-Test-Debug-Command-Line.sh
+++ scripts/403-Test-Debug-Command-Line.sh
@@ -6,14 +6,14 @@
 # See licensing note at end.
 ################################################################################
 
-TEST_DIR="$HOME/403-Test-CPU-Debug-Command-Line"
+TEST_DIR="$TEST_LLAMA_CPP_DIR/$TEST_DEBUG_COMMAND_LINE"
 mkdir -p $TEST_DIR
 rm -r -f $TEST_DIR/*
 cd $TEST_DIR
 
 rm -f mmojo-server-args
 rm -r -f mmojo-server-support
-$BUILD_LLAMA_CPP_DIR/$BUILD_CPU_DEBUG/bin/mmojo-server --model $MODELS_DIR/Google-Gemma-1B-Instruct-v3-q8_0.gguf \
+$BUILD_LLAMA_CPP_DIR/$BUILD_DEBUG/bin/mmojo-server --model $MODELS_DIR/Google-Gemma-1B-Instruct-v3-q8_0.gguf \
     --path $BUILD_LLAMA_CPP_DIR/completion-ui/ --default-ui-endpoint "chat" --host 0.0.0.0 --port 8080 --batch-size 64 \
     --threads-http 8 --ctx-size 0 --mlock
 
@@ -23,7 +23,7 @@ ls -ald mmojo-server-support
 
 cd $HOME
 
-printf "\n**********\n*\n* FINISHED: 403-Test-CPU-Debug-Command-Line.sh.\n*\n**********\n\n"
+printf "\n**********\n*\n* FINISHED: 403-Test-Debug-Command-Line.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
