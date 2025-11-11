a251e72dcc041e141b05d382a0cde8d21a23f589
Brad Hutchings <brad@componentx.com>
2025-11-10 17:44:06 -0800

    Update and rename 404-Build-CPU-Release.sh to 405-Build-CPU-Release.sh

--- scripts/405-Build-CPU-Release.sh
+++ scripts/405-Build-CPU-Release.sh
@@ -32,7 +32,7 @@ printf "\n"
 
 cd $HOME
 
-printf "\n**********\n*\n* FINISHED: 404-Build-CPU-Release.sh.\n*\n**********\n\n"
+printf "\n**********\n*\n* FINISHED: 405-Build-CPU-Release.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
