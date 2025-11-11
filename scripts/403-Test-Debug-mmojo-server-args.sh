b6b5c17518f16f2066286a918829d5b467f028ee
Brad Hutchings <brad@componentx.com>
2025-11-11 06:08:02 -0800

    Update and rename 403-Test-CPU-Debug-mmojo-server-args.sh to 403-Test-Debug-mmojo-server-args.sh

--- scripts/403-Test-Debug-mmojo-server-args.sh
+++ scripts/403-Test-Debug-mmojo-server-args.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ################################################################################
-# This script runs the CPU-Debug build arguments in a `mmojo-server-args` file.
+# This script runs the debug build arguments in a `mmojo-server-args` file.
 #
 # See licensing note at end.
 ################################################################################
 
-TEST_DIR="$HOME/403-Test-CPU-Debug-mmojo-server-args"
+TEST_DIR="$TEST_LLAMA_CPP_DIR/$TEST_DEBUG_MMOJO_SERVER_ARGS"
 mkdir -p $TEST_DIR
 rm -r -f $TEST_DIR/*
 cd $TEST_DIR
@@ -35,7 +35,7 @@ chat
 --mlock
 ...
 EOF
-$BUILD_LLAMA_CPP_DIR/$BUILD_CPU_DEBUG/bin/mmojo-server
+$BUILD_LLAMA_CPP_DIR/$BUILD_DEBUG/bin/mmojo-server
 
 printf "\nVerify that 'mmojo-server-args' exists and 'mmojo-server-support' does not exist.\n"
 ls -ald mmojo-server-args
@@ -43,7 +43,7 @@ ls -ald mmojo-server-support
 
 cd $HOME
 
-printf "\n**********\n*\n* FINISHED: 403-Test-CPU-Debug-mmojo-server-args.sh.\n*\n**********\n\n"
+printf "\n**********\n*\n* FINISHED: 403-Test-Debug-mmojo-server-args.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
