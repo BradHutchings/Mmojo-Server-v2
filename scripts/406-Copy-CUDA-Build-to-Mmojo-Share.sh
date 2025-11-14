242af9b7f33976a315b29635a01760ea15d0a86c
Brad Hutchings <brad@componentx.com>
2025-11-14 05:17:28 -0800

    Create 406-Copy-CUDA-Build-to-Mmojo-Share.sh

---
+++ scripts/406-Copy-CUDA-Build-to-Mmojo-Share.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+################################################################################
+# This script copies the x86_64 build of mmojo-server and mm-zipalign to the
+# right place on the Mmojo Share.
+#
+# See licensing note at end.
+################################################################################
+
+mm-mount-mmojo-share.sh
+
+if [[ $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
+  sudo mkdir -p $MMOJO_SHARE_BUILDS
+  sudo mkdir -p $MMOJO_SHARE_BUILDS_CUDA
+
+  # TO-DO: What CPU architecture? What CPU options?
+
+  if [ -d "$MMOJO_SHARE_BUILDS_CUDA" ]; then
+    echo "Copying mmojo-server-cuda to Mmojo Share."
+    sudo cp -f $BUILD_LLAMA_CPP_DIR/$BUILD_CUDA/bin/mmojo-server $MMOJO_SHARE_BUILDS_CUDA/mmojo-server-cuda
+  fi
+fi
+
+printf "\n**********\n*\n* FINISHED: 406-Copy-CUDA-Build-to-Mmojo-Share.sh.\n*\n**********\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
