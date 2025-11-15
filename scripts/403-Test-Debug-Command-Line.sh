e0c2da84fb11d89fe1d93b5c881066c8502adbdf
Brad Hutchings <brad@componentx.com>
2025-11-15 14:04:57 -0800

    Update and rename 404-Test-Debug-Command-Line.sh to 403-Test-Debug-Command-Line.sh

--- scripts/403-Test-Debug-Command-Line.sh
+++ scripts/403-Test-Debug-Command-Line.sh
@@ -30,7 +30,7 @@ ls -ald mmojo-server-support
 
 cd $HOME
 
-printf "\n**********\n*\n* FINISHED: 404-Test-Debug-Command-Line.sh.\n*\n**********\n\n"
+printf "\n**********\n*\n* FINISHED: 403-Test-Debug-Command-Line.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
