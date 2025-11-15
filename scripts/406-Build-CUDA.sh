7b5323efee5f7cd1b5b6fda04980c0c9e67d63e1
Brad Hutchings <brad@componentx.com>
2025-11-15 14:10:12 -0800

    Update and rename 407-Build-CUDA.sh to 406-Build-CUDA.sh

--- scripts/406-Build-CUDA.sh
+++ scripts/406-Build-CUDA.sh
@@ -32,7 +32,7 @@ printf "\n"
 
 cd $HOME
 
-printf "\n**********\n*\n* FINISHED: 407-Build-CUDA.sh.\n*\n**********\n\n"
+printf "\n**********\n*\n* FINISHED: 406-Build-CUDA.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
