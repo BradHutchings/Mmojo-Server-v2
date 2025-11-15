de7d65197b2f11ce433659c8a1b82738cd1b3882
Brad Hutchings <brad@componentx.com>
2025-11-15 14:16:31 -0800

    Update 404-Copy-Cosmo-APE-to-Mmojo-Share.sh

--- scripts/404-Copy-Cosmo-APE-to-Mmojo-Share.sh
+++ scripts/404-Copy-Cosmo-APE-to-Mmojo-Share.sh
@@ -20,7 +20,9 @@ if [[ $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
   fi
 fi
 
-printf "\n**********\n*\n* FINISHED: 404-Copy-Cosmo-APE-to-Mmojo-Share.sh.\n*\n**********\n\n"
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
+# printf "\n**********\n*\n* FINISHED: 404-Copy-Cosmo-APE-to-Mmojo-Share.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
