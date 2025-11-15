5e47ad9505491a6340ec3fe6c3227883355d035b
Brad Hutchings <brad@componentx.com>
2025-11-15 14:07:23 -0800

    Update and rename 405-Copy-Cosmo-APE-to-Mmojo-Share.sh to 404-Copy-Cosmo-APE-to-Mmojo-Share.sh

--- scripts/404-Copy-Cosmo-APE-to-Mmojo-Share.sh
+++ scripts/404-Copy-Cosmo-APE-to-Mmojo-Share.sh
@@ -20,7 +20,7 @@ if [[ $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
   fi
 fi
 
-printf "\n**********\n*\n* FINISHED: 405-Copy-Cosmo-APE-to-Mmojo-Share.sh.\n*\n**********\n\n"
+printf "\n**********\n*\n* FINISHED: 404-Copy-Cosmo-APE-to-Mmojo-Share.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
