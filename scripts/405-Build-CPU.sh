5320811fd1e7920c33a2a72b89077ae2b82d1ce5
Brad Hutchings <brad@componentx.com>
2025-11-15 14:17:31 -0800

    Update 405-Build-CPU.sh

--- scripts/405-Build-CPU.sh
+++ scripts/405-Build-CPU.sh
@@ -27,7 +27,9 @@ printf "\n"
 
 cd $HOME
 
-printf "\n**********\n*\n* FINISHED: 405-Build-CPU.sh.\n*\n**********\n\n"
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
+# printf "\n**********\n*\n* FINISHED: 405-Build-CPU.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
