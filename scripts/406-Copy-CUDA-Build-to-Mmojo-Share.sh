a4c7f360146214c63103d8868436b3ccbdf48252
Brad Hutchings <brad@componentx.com>
2025-11-15 19:56:19 -0800

    Update 406-Copy-CUDA-Build-to-Mmojo-Share.sh

--- scripts/406-Copy-CUDA-Build-to-Mmojo-Share.sh
+++ scripts/406-Copy-CUDA-Build-to-Mmojo-Share.sh
@@ -7,6 +7,9 @@
 # See licensing note at end.
 ################################################################################
 
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+
 mm-mount-mmojo-share.sh
 
 if [[ $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
@@ -22,9 +25,7 @@ if [[ $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
   fi
 fi
 
-SCRIPT_NAME=$(basename -- "$0")
 printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
-# printf "\n**********\n*\n* FINISHED: 406-Copy-CUDA-Build-to-Mmojo-Share.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
