0d9ed009f9e67725c979531dc20336256a6740b0
Brad Hutchings <brad@componentx.com>
2025-11-16 08:49:12 -0800

    Update mm-install-system-updates.sh

--- scripts/mm-install-system-updates.sh
+++ scripts/mm-install-system-updates.sh
@@ -6,10 +6,13 @@
 # See licensing note at end.
 ################################################################################
 
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+
 sudo apt update
 sudo apt upgrade -y
 
-printf "\n**********\n*\n* FINISHED: mm-install-system-updates.sh.\n*\n**********\n\n"
+printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
