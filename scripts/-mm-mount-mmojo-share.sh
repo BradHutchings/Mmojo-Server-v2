3ade8e0dfc765f200063db117202fae2267f154f
Brad Hutchings <brad@componentx.com>
2025-11-16 08:49:58 -0800

    Update -mm-mount-mmojo-share.sh

--- scripts/-mm-mount-mmojo-share.sh
+++ scripts/-mm-mount-mmojo-share.sh
@@ -16,6 +16,9 @@ HOST="[HOST]"
 USER="[USER]"
 SHARE="mmojo"
 
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+
 if [ "$HOST" == "[HOST]" ]; then
    echo "Your mm-mount-mmojo-share.sh script is not configured."
    exit
@@ -38,6 +41,8 @@ if [[ ! $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
         sudo mount -t cifs -o user=$USER //$HOST/$SHARE $MMOJO_SHARE_MOUNT_POINT
     fi
 fi
+
+printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
