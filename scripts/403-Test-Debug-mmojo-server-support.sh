f538ee5cdfcd4572cab169ba24ea8ba59b02e8dc
Brad Hutchings <brad@componentx.com>
2025-11-15 19:50:51 -0800

    Update 403-Test-Debug-mmojo-server-support.sh

--- scripts/403-Test-Debug-mmojo-server-support.sh
+++ scripts/403-Test-Debug-mmojo-server-support.sh
@@ -7,6 +7,9 @@
 # See licensing note at end.
 ################################################################################
 
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+
 TEST_DIR="$TEST_LLAMA_CPP_DIR/$TEST_DEBUG_MMOJO_SERVER_SUPPORT"
 mkdir -p $TEST_DIR
 rm -r -f $TEST_DIR/*
@@ -51,9 +54,7 @@ ls -ald mmojo-server-support
 
 cd $HOME
 
-SCRIPT_NAME=$(basename -- "$0")
 printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
-# printf "\n**********\n*\n* FINISHED: 403-Test-Debug-mmojo-server-support.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
