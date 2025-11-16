c02bbbceed63bdd0a79e9c82c692999329c0e290
Brad Hutchings <brad@componentx.com>
2025-11-15 19:57:07 -0800

    Update 407-Build-Vulkan.sh

--- scripts/407-Build-Vulkan.sh
+++ scripts/407-Build-Vulkan.sh
@@ -10,6 +10,9 @@
 # See licensing note at end.
 ################################################################################
 
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+
 cd $BUILD_LLAMA_CPP_DIR
 
 if [ "$BUILD_PROFILE" ]; then
@@ -32,9 +35,7 @@ printf "\n"
 
 cd $HOME
 
-SCRIPT_NAME=$(basename -- "$0")
 printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
-# printf "\n**********\n*\n* FINISHED: 407-Build-Vulkan.sh.\n*\n**********\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
