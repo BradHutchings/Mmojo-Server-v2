8cf938df59c8d265210712898a9cb5e0a18bd025
Brad Hutchings <brad@componentx.com>
2025-11-16 15:18:22 -0800

    Update 403-Test-Debug-Command-Line.sh

--- scripts/403-Test-Debug-Command-Line.sh
+++ scripts/403-Test-Debug-Command-Line.sh
@@ -14,12 +14,13 @@ mkdir -p $TEST_DIR
 rm -r -f $TEST_DIR/*
 cd $TEST_DIR
 
-UI_PARAMS=" --path $BUILD_LLAMA_CPP_DIR/Mmojo-Complete/ --default-ui-endpoint /chat "
-if [ ! -z $TEST_WITH_CHAT_UI ] && [ $TEST_WITH_CHAT_UI != 0 ]; then 
-    # echo "Using chat UI."
-    UI_PARAMS=""
+MODEL_PARAM="Google-Gemma-1B-Instruct-v3-q8_0.gguf"
+if [[ -v TEST_MODEL ]]; then
+    if [ -f "$MODELS_DIR/$TEST_MODEL" ]; then
+        MODEL_PARAM=$TEST_MODEL
+    fi
 fi
-# echo "\$UI_PARAMS: $UI_PARAMS"
+# echo "\$MODEL_PARAM: $MODEL_PARAM"
 # sleep 5s
 
 THREADS_PARAM=""
@@ -29,9 +30,17 @@ fi
 # echo "\$THREADS_PARAM: $THREADS_PARAM"
 # sleep 5s
 
+UI_PARAMS=" --path $BUILD_LLAMA_CPP_DIR/Mmojo-Complete/ --default-ui-endpoint /chat "
+if [ ! -z $TEST_WITH_CHAT_UI ] && [ $TEST_WITH_CHAT_UI != 0 ]; then 
+    # echo "Using chat UI."
+    UI_PARAMS=""
+fi
+# echo "\$UI_PARAMS: $UI_PARAMS"
+# sleep 5s
+
 rm -f mmojo-server-args
 rm -r -f mmojo-server-support
-$BUILD_LLAMA_CPP_DIR/$BUILD_DEBUG/bin/mmojo-server --model $MODELS_DIR/Google-Gemma-1B-Instruct-v3-q8_0.gguf \
+$BUILD_LLAMA_CPP_DIR/$BUILD_DEBUG/bin/mmojo-server --model $MODELS_DIR/$MODEL_PARAM \
     $UI_PARAMS $THREADS_PARAM --host 0.0.0.0 --port 8080 --batch-size 64 --threads-http 8 --ctx-size 0 --mlock
 
 printf "\nVerify that 'mmojo-server-args' and 'mmojo-server-support' do not exist.\n"
