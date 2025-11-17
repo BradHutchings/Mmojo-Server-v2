69f5c7973ebca9d7fadc250fba480b6568b6ae72
Brad Hutchings <brad@componentx.com>
2025-11-16 20:31:41 -0800

    Update 407-Build-Vulkan.sh

--- scripts/407-Build-Vulkan.sh
+++ scripts/407-Build-Vulkan.sh
@@ -13,7 +13,7 @@
 SCRIPT_NAME=$(basename -- "$0")
 printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
 
-cd $BUILD_LLAMA_CPP_DIR
+cd $BUILD_DIR
 
 if [ "$BUILD_PROFILE" ]; then
     export CC="cc -pg "
@@ -22,15 +22,15 @@ fi
 
 # TO-DO: Some way to add -DCMAKE_VERBOSE_MAKEFILE=ON  on the fly to all these.
 
-rm -r -f $BUILD_LLAMA_CPP_DIR/$BUILD_VULKAN
+rm -r -f $BUILD_DIR/$BUILD_VULKAN
 cmake -B $BUILD_VULKAN -DBUILD_SHARED_LIBS=OFF -DLLAMA_CURL=OFF -DLLAMA_OPENSSL=ON \
     -DCMAKE_BUILD_TYPE=Release -DGGML_VULKAN=ON # -DCMAKE_VERBOSE_MAKEFILE=ON 
 cmake --build $BUILD_VULKAN
 
 # Show off what we built
 printf "\nBuild of llama.cpp with Vulkan support is complete.\n\n"
-printf "\$ ls -al $BUILD_LLAMA_CPP_DIR/$BUILD_VULKAN/bin/\n"
-ls -al $BUILD_LLAMA_CPP_DIR/$BUILD_VULKAN/bin
+printf "\$ ls -al $BUILD_DIR/$BUILD_VULKAN/bin/\n"
+ls -al $BUILD_DIR/$BUILD_VULKAN/bin
 printf "\n"
 
 cd $HOME
