38ea7350794bcf4abf149188ed0a6f0446be2dfe
Brad Hutchings <brad@componentx.com>
2025-11-18 08:23:13 -0800

    Update 407-Copy-Vulkan-Build-to-Mmojo-Share.sh

--- scripts/407-Copy-Vulkan-Build-to-Mmojo-Share.sh
+++ scripts/407-Copy-Vulkan-Build-to-Mmojo-Share.sh
@@ -8,7 +8,7 @@
 ################################################################################
 
 SCRIPT_NAME=$(basename -- "$0")
-printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 mm-mount-mmojo-share.sh
 
@@ -26,7 +26,7 @@ if [[ $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
   fi
 fi
 
-printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
