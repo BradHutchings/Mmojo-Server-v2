e94cb841bffcabedab723cdabb5676b6b164e1b3
Brad Hutchings <brad@componentx.com>
2025-11-18 08:40:53 -0800

    Update 206-Create-Mmojo-Share-Mount-Point.sh

--- scripts/206-Create-Mmojo-Share-Mount-Point.sh
+++ scripts/206-Create-Mmojo-Share-Mount-Point.sh
@@ -7,13 +7,17 @@
 ################################################################################
 
 SCRIPT_NAME=$(basename -- "$0")
-printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
+#--------------------------------------------------------------------------------
+# Haven't installed system updates and dependencies yet.
+#--------------------------------------------------------------------------------
 
 echo "Installing cifs-utils."
 sudo apt install -y cifs-utils
 
 #----------------------------------------
-# Create mpount point: /mnt/mmojo
+# Create mount point: /mnt/mmojo
 #----------------------------------------
 if [ ! -d "$MMOJO_SHARE_MOUNT_POINT" ]; then
     echo "Creating Mmojo Share mount point - $MMOJO_SHARE_MOUNT_POINT."
@@ -27,7 +31,7 @@ echo "Copying $MOUNT_MMOJO_SHARE_SCRIPT script."
 cp "$MMOJO_SERVER_SCRIPTS/-$MOUNT_MMOJO_SHARE_SCRIPT" "$HOME_SCRIPTS/$MOUNT_MMOJO_SHARE_SCRIPT"
 chmod a+x "$HOME_SCRIPTS/$MOUNT_MMOJO_SHARE_SCRIPT"
 
-printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
