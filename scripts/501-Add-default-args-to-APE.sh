a8d8d8960bccb4d53d3c108ca951320617b20988
Brad Hutchings <brad@componentx.com>
2025-11-18 11:13:01 -0800

    Create 501-Add-default-args-to-APE.sh

---
+++ scripts/501-Add-default-args-to-APE.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+################################################################################
+# This script adds certs from the Mmojo Share to the mmojo-server.zip packaging
+# file.
+#
+# See licensing note at end.
+################################################################################
+
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+
+PACKAGING_ZIP_FILE="$PACKAGE_DIR/$PACKAGE_APE/$PACKAGE_MMOJO_SERVER_ZIP_FILE"
+
+cd "$PACKAGE_DIR/$PACKAGE_APE"
+cat << EOF > $PACKAGE_DEFAULT_ARGS_FILE
+--host
+127.0.0.1
+--port
+8080
+--ctx-size
+0
+--threads-http
+8
+--batch-size
+64
+--batch-sleep-ms
+0
+EOF
+
+if [ $ADDED_CERTS ]; then
+cat << EOF >> $PACKAGE_DEFAULT_ARGS_FILE
+--ssl-key-file
+/zip/certs/mmojo.local.key
+--ssl-cert-file
+/zip/certs/mmojo.local.crt
+EOF
+fi
+
+if [ $ADDED_MMOJO_COMPLETE ]; then
+cat << EOF >> $PACKAGE_DEFAULT_ARGS_FILE
+--path
+/zip/website
+--default-ui-endpoint
+chat
+EOF
+fi
+
+if [ $ADDED_MODEL ]; then
+cat << EOF >> $PACKAGE_DEFAULT_ARGS_FILE
+--model
+SELECTED_MODEL
+EOF
+
+cat << EOF >> $PACKAGE_DEFAULT_ARGS_FILE
+...
+EOF
+
+zip -0 -r $MMOJO_SERVER_ZIP $PACKAGE_DEFAULT_ARGS_FILE
+
+cd $HOME
+
+printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
