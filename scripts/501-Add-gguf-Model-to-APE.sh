2d03b037f9ee3bd6ea94935fbf55046eada54d23
Brad Hutchings <brad@componentx.com>
2025-11-18 10:06:36 -0800

    Create 501-Add-gguf-Model-to-APE.sh

---
+++ scripts/501-Add-gguf-Model-to-APE.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+################################################################################
+# This script adds certs from the Mmojo Share to the mmojo-server.zip packaging
+# file.
+#
+# See licensing note at end.
+################################################################################
+
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+
+PACKAGING_ZIP_FILE="$PACKAGE_DIR/$PACKAGE_APE/$PACKAGE_MMOJO_SERVER_ZIP_FILE"
+
+if [ -v $SELECTED_MODEL ]; then
+  echo "Selected model: $SELECTED_MODEL"
+  MODEL_FILE="$MODELS_DIR/$SELECTED_MODEL"
+  if [ -f "$MODEL_FILE" ]; then
+    echo "mm-zipalign-ing $MODEL_FILE."
+    $ZIPALIGN $PACKAGING_ZIP_FILE $MODEL_FILE
+  fi
+fi
+
+echo ""
+echo "Contents of $PACKAGE_MMOJO_SERVER_APE_FILE:"
+unzip -l $PACKAGING_ZIP_FILE 
+
+cd $HOME
+
+printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
