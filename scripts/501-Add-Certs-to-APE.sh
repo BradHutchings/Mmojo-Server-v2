9720dfad0264a12e75ec12b5bfee726d0a6bb066
Brad Hutchings <brad@componentx.com>
2025-11-18 09:19:15 -0800

    Update 501-Add-Certs-to-APE.sh

--- scripts/501-Add-Certs-to-APE.sh
+++ scripts/501-Add-Certs-to-APE.sh
@@ -10,6 +10,8 @@
 SCRIPT_NAME=$(basename -- "$0")
 printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
 
+PACKAGING_ZIP_FILE="$PACKAGE_DIR/$PACKAGE_APE/$PACKAGE_MMOJO_SERVER_ZIP_FILE"
+
 if [[ ! $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
   mm-mount-mmojo-share.sh
 fi
@@ -20,7 +22,7 @@ if [[ ! $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
   cp $MMOJO_SHARE_MOUNT_POINT/Mmojo-certs/mmojo.local.crt $CERTS
   cp $MMOJO_SHARE_MOUNT_POINT/Mmojo-certs/mmojo.local.key  $CERTS
   cp $MMOJO_SHARE_MOUNT_POINT/Mmojo-certs/selfsignCA.crt $CERTS
-  zip -0 -r $MMOJO_SERVER_ZIP certs/*
+  zip -0 -r $PACKAGING_ZIP_FILE certs/*
 fi
 
 echo ""
