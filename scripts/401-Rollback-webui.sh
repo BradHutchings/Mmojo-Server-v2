0233432e8c0e39ccca2f616615421a7e76223e74
Brad Hutchings <brad@componentx.com>
2025-11-18 08:30:26 -0800

    Update 401-Rollback-webui.sh

--- scripts/401-Rollback-webui.sh
+++ scripts/401-Rollback-webui.sh
@@ -9,7 +9,7 @@
 ################################################################################
 
 SCRIPT_NAME=$(basename -- "$0")
-printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 cd $BUILD_DIR
 
@@ -22,7 +22,7 @@ sed -i -e "s/>.*llama.cpp.*</>$APP_NAME</g" tools/server/webui/src/components/He
 
 cd $HOME
 
-printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
