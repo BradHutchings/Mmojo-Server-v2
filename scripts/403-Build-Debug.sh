c328ad247e54b57ae7b4b4314dae6e353f736030
Brad Hutchings <brad@componentx.com>
2025-11-18 08:30:06 -0800

    Update 403-Build-Debug.sh

--- scripts/403-Build-Debug.sh
+++ scripts/403-Build-Debug.sh
@@ -11,7 +11,7 @@
 ################################################################################
 
 SCRIPT_NAME=$(basename -- "$0")
-printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 cd $BUILD_DIR
 
@@ -30,7 +30,7 @@ printf "\n"
 
 cd $HOME
 
-printf "\n**********\n*\n* FINISHED: $SCRIPT_NAME.\n*\n**********\n\n"
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 ################################################################################
 #  This is an original script for the Mmojo Server repo. It is covered by
