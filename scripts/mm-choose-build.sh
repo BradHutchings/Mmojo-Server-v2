01cb9888946fa77517bf0a6cb5a66e2ffa9e9099
Brad Hutchings <brad@componentx.com>
2025-11-20 13:39:30 -0800

    Update mm-choose-build.sh

--- scripts/mm-choose-build.sh
+++ scripts/mm-choose-build.sh
@@ -9,49 +9,82 @@
 SCRIPT_NAME=$(basename -- "$0")
 # printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
 
-mmojo_servers=() 
+unset CHOSEN_BUILD
+unset CHOSEN_BUILD_PATH
 
-ms="$BUILD_DIR/$BUILD_DEBUG/bin/mmojo-server"
-if [ -f "$ms" ]; then
-  echo "Adding debug."
-  mmojo_servers+=($ms)
-fi
+build_paths=()
+build_names=()
+build_paths+=("$BUILD_DIR/$BUILD_DEBUG/bin/mmojo-server")
+build_paths+=("$BUILD_DIR/$BUILD_COSMO_APE/mmojo-server-ape")
+build_paths+=("$BUILD_DIR/$BUILD_CPU/bin/mmojo-server")
+build_paths+=("$BUILD_DIR/$BUILD_CUDA/bin/mmojo-server")
+build_paths+=("$BUILD_DIR/$BUILD_VULKAN/bin/mmojo-server")
+build_paths+=("$BUILD_DIR/$BUILD_METAL/bin/mmojo-server")
 
-ms="$BUILD_DIR/$BUILD_COSMO_APE/mmojo-server-ape"
-if [ -f "$ms" ]; then
-  echo "Adding ape."
-  mmojo_servers+=($ms)
-fi
+build_names+=("DEBUG")
+build_names+=("APE")
+build_names+=("CPU")
+build_names+=("CUDA")
+build_names+=("VULKAN")
+build_names+=("METAL")
 
-ms="$BUILD_DIR/$BUILD_CPU/bin/mmojo-server"
-if [ -f "$ms" ]; then
-  echo "Adding cpu."
-  mmojo_servers+=($ms)
-fi
-
-ms="$BUILD_DIR/$BUILD_CUDA/bin/mmojo-server"
-if [ -f "$ms" ]; then
-  echo "Adding cuda."
-  mmojo_servers+=($ms)
-fi
-
-ms="$BUILD_DIR/$BUILD_VULKAN/bin/mmojo-server"
-if [ -f "$ms" ]; then
-  echo "Adding vulkan."
-  mmojo_servers+=($ms)
-fi
-
-ms="$BUILD_DIR/$BUILD_METAL/bin/mmojo-server"
-if [ -f "$ms" ]; then
-  echo "Adding metal."
-  mmojo_servers+=($ms)
-fi
-
-i=1
-for ms in "${mmojo_servers[@]}"; do
-  echo "$i. $ms"
-  i=$(($i + 1))
+available_build_paths=()
+available_build_names=()
+i=0
+for build_path in "${build_paths[@]}"; do
+  if [ -f "${build_path}" ]; then
+    # echo "$i. $build_path -- ${build_names[$i]}"
+    available_build_paths+=($build_path)
+    available_build_names+=(${build_names[$i]})
+  fi
+  i=$((i+1))
 done
+
+# List the available builds and their paths.
+if false; then
+  i=0
+  for build_path in "${available_build_paths[@]}"; do
+    echo "$i. ${available_build_names[$i]} -- $build_path"
+    i=$((i+1))
+  done
+fi
+
+PS3="Please choose a build:"
+select choice in "${available_build_names[@]}"; do
+  export CHOSEN_BUILD=$choice
+  case $choice in
+    "DEBUG")
+      export CHOSEN_BUILD_PATH=${build_paths[0]}
+      break
+      ;;
+    "APE")
+      export CHOSEN_BUILD_PATH=${build_paths[1]}
+      break
+      ;;
+    "CPU")
+      export CHOSEN_BUILD_PATH=${build_paths[2]}
+      break
+      ;;
+    "CUDA")
+      export CHOSEN_BUILD_PATH=${build_paths[3]}
+      break
+      ;;
+    "VULKAN")
+      export CHOSEN_BUILD_PATH=${build_paths[4]}
+      break
+      ;;
+    "METAL")
+      export CHOSEN_BUILD_PATH=${build_paths[5]}
+      break
+      ;;
+    *)
+      unset CHOSEN_BUILD
+      unset CHOSEN_BUILD_PATH
+      break
+  esac
+done
+
+# echo "$CHOSEN_BUILD -- $CHOSEN_BUILD_PATH"
 
 cd $HOME
 
