794cc752b42e17ac558fec44e971890015a79c24
Brad Hutchings <brad@componentx.com>
2025-11-20 18:18:43 -0800

    Update 501-Add-Certs-to-APE.sh

--- scripts/501-Add-Certs-to-APE.sh
+++ scripts/501-Add-Certs-to-APE.sh
@@ -10,26 +10,31 @@
 SCRIPT_NAME=$(basename -- "$0")
 printf "\n**********\n*\n* STARTED: $SCRIPT_NAME.\n*\n**********\n\n"
 
-PACKAGING_ZIP_FILE="$PACKAGE_DIR/$PACKAGE_APE/$PACKAGE_MMOJO_SERVER_ZIP_FILE"
+THIS_PACKAGE_DIR="$PACKAGE_DIR/$PACKAGE_APE"
+if [ -v CHOSEN_SHORT_NAME ]; then
+    THIS_PACKAGE_DIR+="-$CHOSEN_SHORT_NAME"
+fi
+
+ZIP_FILE="$THIS_PACKAGE_DIR/$PACKAGE_MMOJO_SERVER_ZIP_FILE"
 
 if [[ ! $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
   mm-mount-mmojo-share.sh
 fi
 
 if [[ $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
-  CERTS="$PACKAGE_DIR/$PACKAGE_APE/certs"
+  CERTS="$THIS_PACKAGE_DIR/certs"
   mkdir -p $CERTS
   cp $MMOJO_SHARE_MOUNT_POINT/Mmojo-certs/mmojo.local.crt $CERTS
   cp $MMOJO_SHARE_MOUNT_POINT/Mmojo-certs/mmojo.local.key  $CERTS
   cp $MMOJO_SHARE_MOUNT_POINT/Mmojo-certs/selfsignCA.crt $CERTS
 
-  cd $PACKAGE_DIR/$PACKAGE_APE
-  zip -0 -r $PACKAGING_ZIP_FILE certs/*
+  cd $THIS_PACKAGE_DIR
+  zip -0 -r $ZIP_FILE certs/*
 fi
 
 echo ""
-echo "Contents of $PACKAGING_ZIP_FILE:"
-unzip -l $PACKAGING_ZIP_FILE 
+echo "Contents of $ZIP_FILE:"
+unzip -l $ZIP_FILE 
 
 cd $HOME
 
