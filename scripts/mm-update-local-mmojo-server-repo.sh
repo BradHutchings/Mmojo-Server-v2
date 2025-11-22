3d237a31fb52af4ae7dd970b039dae3e7f778195
Brad Hutchings <brad@componentx.com>
2025-11-21 16:44:14 -0800

    Update mm-update-local-mmojo-server-repo.sh

--- scripts/mm-update-local-mmojo-server-repo.sh
+++ scripts/mm-update-local-mmojo-server-repo.sh
@@ -21,6 +21,7 @@ if [ -d "$MMOJO_SERVER_DIR" ]; then
   chmod a+x $MMOJO_SERVER_SCRIPTS/3*.sh
   chmod a+x $MMOJO_SERVER_SCRIPTS/4*.sh
   chmod a+x $MMOJO_SERVER_SCRIPTS/5*.sh
+  chmod a+x $MMOJO_SERVER_SCRIPTS/14*.sh
 
   ### Links don't work - end up modifying repo files on chmod.
   cp $MMOJO_SERVER_SCRIPTS/mm-*.sh $HOME_SCRIPTS
