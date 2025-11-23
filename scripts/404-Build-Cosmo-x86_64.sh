5065e27135e225bc6c8d4168407e4cabbda3bd20
Brad Hutchings <brad@componentx.com>
2025-11-22 21:44:04 -0800

    Update 404-Build-Cosmo-x86_64.sh

--- scripts/404-Build-Cosmo-x86_64.sh
+++ scripts/404-Build-Cosmo-x86_64.sh
@@ -71,7 +71,7 @@ echo ""
 # export CC="x86_64-unknown-cosmo-cc -I$(pwd)/cosmocc/include -L$(pwd)/cosmocc/lib \
 #     -DCOSMOCC=1 -nostdinc -O3 $X86_64_ARCH_LEVEL_PARAM"
 # export CXX="x86_64-unknown-cosmo-c++ -I$(pwd)/cosmocc/include \
-#     -DCOSMOCC=1 -nostdinc -nostdinc++ -O3 -Wno-format-truncation  $X86_64_ARCH_LEVEL_PARAM \
+#     -DCOSMOCC=1 -nostdinc -nostdinc++ -O3 -Wno-format-truncation $X86_64_ARCH_LEVEL_PARAM \
 #     -I$(pwd)/cosmocc/include/third_party/libcxx \
 #     -I$(pwd)/openssl/include \
 #     -L$(pwd)/cosmocc/lib -L$(pwd)/openssl"
@@ -84,7 +84,7 @@ export PATH="$(pwd)/cosmocc/bin:$SAVE_PATH"
 export CC="x86_64-unknown-cosmo-cc \
     -DCOSMOCC=1 -nostdinc -O3 $X86_64_ARCH_LEVEL_PARAM"
 export CXX="x86_64-unknown-cosmo-c++ \
-    -DCOSMOCC=1 -nostdinc -nostdinc++ -O3 -Wno-format-truncation  $X86_64_ARCH_LEVEL_PARAM  \
+    -DCOSMOCC=1 -nostdinc -nostdinc++ -O3 -Wno-format-truncation $X86_64_ARCH_LEVEL_PARAM  \
     -I$(pwd)/openssl/include \
     -L$(pwd)/openssl"
 export AR="cosmoar"
