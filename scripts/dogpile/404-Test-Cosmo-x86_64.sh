e715dec76b8209fa96c8595d22330266bbb53626
Brad Hutchings <brad@componentx.com>
2025-11-23 11:39:58 -0800

    Update 404-Test-Cosmo-x86_64.sh

--- scripts/dogpile/404-Test-Cosmo-x86_64.sh
+++ scripts/dogpile/404-Test-Cosmo-x86_64.sh
@@ -32,15 +32,21 @@ fi
 # echo "\$THREADS_PARAM: $THREADS_PARAM"
 # sleep 5s
 
-rm -f mmojo-server-args
-rm -r -f mmojo-server-support
+printf "\nFIX THIS: Need to be $PACKAGE_DOGPILE_ARGS_FILE and $PACKAGE_MMOJO_SERVER_SUPPORT_DIR in server.mmojo.cpp - Patch that.\n"
+rm -f $PACKAGE_MMOJO_SERVER_ARGS_FILE
+rm -r -f $PACKAGE_MMOJO_SERVER_SUPPORT_DIR
+# rm -f $PACKAGE_DOGPILE_ARGS_FILE
+# rm -r -f $PACKAGE_MMOJO_SERVER_SUPPORT_DIR
+
 $DOGPILE_BUILD_DIR/$BUILD_COSMO_X86_64/bin/dogpile --model $MODELS_DIR/$MODEL_PARAM \
     $THREADS_PARAM --host 0.0.0.0 --port 8080 --batch-size 64 --threads-http 8 --ctx-size 0
 
-printf "\nFIX THIS: Need to be 'dogpile-args' and 'dogpile-support' in server.mmojo.cpp - Patch that.\n"
-printf "\nVerify that 'mmojo-server-args' and 'mmojo-server-support' do not exist.\n"
-ls -ald mmojo-server-args
-ls -ald mmojo-server-support
+printf "\nFIX THIS: Need to be $PACKAGE_DOGPILE_ARGS_FILE and $PACKAGE_MMOJO_SERVER_SUPPORT_DIR in server.mmojo.cpp - Patch that.\n"
+printf "\nVerify that args file and support folder do not exist.\n"
+ls -ald $PACKAGE_MMOJO_SERVER_ARGS_FILE
+ls -ald $PACKAGE_MMOJO_SERVER_SUPPORT_DIR
+# ls -ald $PACKAGE_DOGPILE_ARGS_FILE
+# ls -ald $PACKAGE_MMOJO_SERVER_SUPPORT_DIR
 
 cd $HOME
 
