bfb64f1f806c421a28c5c5a6d9c9f9a82a7250e0
Brad Hutchings <brad@componentx.com>
2025-11-22 21:16:12 -0800

    Update 404-Build-Cosmo-x86_64.sh

--- scripts/404-Build-Cosmo-x86_64.sh
+++ scripts/404-Build-Cosmo-x86_64.sh
@@ -75,15 +75,14 @@ export CXX="x86_64-unknown-cosmo-c++ -I$(pwd)/cosmocc/include \
     -L$(pwd)/cosmocc/lib -L$(pwd)/openssl"
 export AR="cosmoar"
 
-export CC="x86_64-unknown-cosmo-cc  \
+# Recent discovery -- cosmo-cc and cosmo-c++ can figure out the -I and -L related to cosmo.
+# No need to specify them here.
+export CC="x86_64-unknown-cosmo-cc \
     -DCOSMOCC=1 -nostdinc -O3 $X86_64_ARCH_LEVEL_PARAM"
-export CXX="x86_64-unknown-cosmo-c++ -I$(pwd)/cosmocc/include \
+export CXX="x86_64-unknown-cosmo-c++ \
     -DCOSMOCC=1 -nostdinc -nostdinc++ -O3 -Wno-format-truncation  $X86_64_ARCH_LEVEL_PARAM  \
     -I$(pwd)/openssl/include \
-     -L$(pwd)/openssl"
-
-
-
+    -L$(pwd)/openssl"
 
 # The OpenSSL linking got moved.
 cp vendor/cpp-httplib/CMakeLists.txt vendor/cpp-httplib/CMakeLists-orig.txt
