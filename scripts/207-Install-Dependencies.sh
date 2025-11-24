877a9f877e30b912a1248df97ccc5771f9562cf9
Brad Hutchings <brad@componentx.com>
2025-11-24 11:38:55 -0800

    Update 207-Install-Dependencies.sh

--- scripts/207-Install-Dependencies.sh
+++ scripts/207-Install-Dependencies.sh
@@ -26,7 +26,15 @@ sudo apt install -y \
     python3 python3-pip python3-tk qt5-qmake qtbase5-dev \
     qtbase5-dev-tools qtcreator tk-dev wayland-protocols xz-utils \
     zip zlib1g-dev cifs-utils
-    
+
+echo ""
+echo "Reinstalling nodejs and npm because recompiling the current llama.cpp webui doesn't like what we have."
+
+sudo apt remove nodejs npm -y
+sudo apt install nodejs npm -y
+sudo npm install -g node@latest
+sudo npm install -g npm@latest
+
 printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 ################################################################################
