77d7053e8c5a9672cdd9bb3ce8b651feb0941a81
Brad Hutchings <brad@componentx.com>
2025-11-23 17:44:24 -0800

    Update 207-Install-Dependencies.sh

--- scripts/207-Install-Dependencies.sh
+++ scripts/207-Install-Dependencies.sh
@@ -21,19 +21,15 @@ sudo apt install -y \
     liblz4-dev liblzma-dev libpciaccess0 libpng-dev libreadline-dev \
     libsqlite3-dev libssl-dev libwayland-dev libx11-dev libx11-xcb-dev \
     libxcb-cursor-dev libxcb-dri3-0 libxcb-dri3-dev libxcb-ewmh-dev libxcb-keysyms1-dev \
-    libxcb-present0 libxcb-randr0-dev libxcb-xinerama0 libxcb-xinput0 libxml2-dev
-
-# The pink screen is triggered by one of these:
-#    libxrandr-dev libzstd-dev ninja-build npm ocaml-core \
-#    pkg-config python3 python3-pip python3-tk qt5-qmake \
-
-sudo apt install -y \
-    libxrandr-dev libzstd-dev ninja-build npm ocaml-core \
-    pkg-config python3 python3-pip python3-tk qt5-qmake
-
-sudo apt install -y \
-    qtbase5-dev qtbase5-dev-tools qtcreator tk-dev wayland-protocols \
-    xz-utils zip zlib1g-dev cifs-utils
+    libxcb-present0 libxcb-randr0-dev libxcb-xinerama0 libxcb-xinput0 libxml2-dev \
+    libxrandr-dev libzstd-dev ninja-build npm pkg-config \
+    python3 python3-pip python3-tk qt5-qmake qtbase5-dev \
+    qtbase5-dev-tools qtcreator tk-dev wayland-protocols xz-utils \
+    zip zlib1g-dev cifs-utils
+    
+# The pink screen is triggered by this one. Let's figure out if it's needed.
+# sudo apt install -y \
+#    ocaml-core 
 
 printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
