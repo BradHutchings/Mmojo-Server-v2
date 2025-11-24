3c26562317af10a6df1d5d2c6366dfa0b859e73f
Brad Hutchings <brad@componentx.com>
2025-11-24 14:01:22 -0800

    Update 404-Build-Cosmo-Compatible-aarch64.sh

--- scripts/404-Build-Cosmo-Compatible-aarch64.sh
+++ scripts/404-Build-Cosmo-Compatible-aarch64.sh
@@ -12,43 +12,7 @@ printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 cd $BUILD_DIR
 
-unset AARCH64_ARCH_LEVEL_PARAM
-if [ -v CPU_PERFORMANCE_LEVEL ]; then
-  case $CPU_PERFORMANCE_LEVEL in
-    1)
-        unset AARCH64_ARCH_LEVEL_PARAM
-        ;;
-    2)
-        AARCH64_ARCH_LEVEL_PARAM=" -march=armv8-a " 
-        ;;
-    3)
-        AARCH64_ARCH_LEVEL_PARAM=" -march=armv8-a "
-        ;;
-    4)
-        AARCH64_ARCH_LEVEL_PARAM=" -march=armv8-a "
-        ;;
-    *)
-        AARCH64_ARCH_LEVEL_PARAM=" -march=armv8.4-a "
-        ;;
-  esac
-fi
-
-echo ""
-echo "\$CPU_PERFORMANCE_LEVEL: $CPU_PERFORMANCE_LEVEL"
-echo "\$AARCH64_ARCH_LEVEL_PARAM: $AARCH64_ARCH_LEVEL_PARAM"
-echo ""
-
-# NOTE: I think I used to have to specify the include and library paths. Maybe before splitting into x86_64 and aarch64 buidls.
-# export PATH="$(pwd)/cosmocc/bin:$SAVE_PATH"
-# export CC="aarch64-unknown-cosmo-cc -I$(pwd)/cosmocc/include -L$(pwd)/cosmocc/lib \
-#    -DCOSMOCC=1 -nostdinc -O3 $AARCH64_ARCH_LEVEL_PARAM"
-# export CXX="aarch64-unknown-cosmo-c++ -I$(pwd)/cosmocc/include \
-#    -DCOSMOCC=1 -nostdinc -nostdinc++ -O3 -Wno-format-truncation $AARCH64_ARCH_LEVEL_PARAM \
-#    -I$(pwd)/cosmocc/include/third_party/libcxx \
-#    -I$(pwd)/cosmocc/include/third_party/libcxx \
-#    -I$(pwd)/openssl/include \
-#    -L$(pwd)/cosmocc/lib -L$(pwd)/openssl/.aarch64/"
-#export AR="cosmoar"
+unset AARCH64_ARCH_LEVEL_PARAM=" -march=armv8-a " 
 
 export PATH="$(pwd)/cosmocc/bin:$SAVE_PATH"
 
@@ -78,8 +42,8 @@ sed -i -e '/error bad version/d' vendor/cpp-httplib/CMakeLists.txt
 # 
 
 # Prepare the build folder
-rm -r -f $BUILD_DIR/$BUILD_COSMO_AARCH64
-cmake -B $BUILD_COSMO_AARCH64 -DBUILD_SHARED_LIBS=OFF -DLLAMA_CURL=OFF -DLLAMA_OPENSSL=ON \
+rm -r -f $BUILD_DIR/$BUILD_COSMO_COMPATIBLE_AARCH64
+cmake -B $BUILD_COSMO_COMPATIBLE_AARCH64 -DBUILD_SHARED_LIBS=OFF -DLLAMA_CURL=OFF -DLLAMA_OPENSSL=ON \
     -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCOSMOCC=1
 
 # Revert to original CMake system.
@@ -90,12 +54,12 @@ cmake -B $BUILD_COSMO_AARCH64 -DBUILD_SHARED_LIBS=OFF -DLLAMA_CURL=OFF -DLLAMA_O
 mv vendor/cpp-httplib/CMakeLists-orig.txt vendor/cpp-httplib/CMakeLists.txt
 
 # Build
-cmake --build $BUILD_COSMO_AARCH64 --config Release
+cmake --build $BUILD_COSMO_COMPATIBLE_AARCH64 --config Release
 
 # Show off what we built
 printf "\nBuild of Cosmo aarch64 of llama.cpp is complete.\n\n"
-printf "\$ ls -al $BUILD_DIR/$BUILD_COSMO_AARCH64/bin/\n"
-ls -al $BUILD_DIR/$BUILD_COSMO_AARCH64/bin
+printf "\$ ls -al $BUILD_DIR/$BUILD_COSMO_COMPATIBLE_AARCH64/bin/\n"
+ls -al $BUILD_DIR/$BUILD_COSMO_COMPATIBLE_AARCH64/bin
 printf "\n"
 
 export PATH=$SAVE_PATH
