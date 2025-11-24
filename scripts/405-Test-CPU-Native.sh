bd9ad9ab88206d2ad75a2918d99d511f833c1de7
Brad Hutchings <brad@componentx.com>
2025-11-23 21:02:42 -0800

    Update 405-Test-CPU-Native.sh

--- scripts/405-Test-CPU-Native.sh
+++ scripts/405-Test-CPU-Native.sh
@@ -40,14 +40,15 @@ fi
 # echo "\$UI_PARAMS: $UI_PARAMS"
 # sleep 5s
 
-rm -f mmojo-server-args
-rm -r -f mmojo-server-support
+rm -f $PACKAGE_MMOJO_SERVER_ARGS_FILE
+rm -r -f $PACKAGE_MMOJO_SERVER_SUPPORT_DIR
+
 $BUILD_DIR/$BUILD_CPU_NATIVE/bin/mmojo-server --model $MODELS_DIR/$MODEL_PARAM \
     $UI_PARAMS $THREADS_PARAM --host 0.0.0.0 --port 8080 --batch-size 64 --threads-http 8 --ctx-size 0
     
 printf "\nVerify that 'mmojo-server-args' and 'mmojo-server-support' do not exist.\n"
-ls -ald mmojo-server-args
-ls -ald mmojo-server-support
+ls -ald $PACKAGE_MMOJO_SERVER_ARGS_FILE
+ls -ald $PACKAGE_MMOJO_SERVER_SUPPORT_DIR
 
 cd $HOME
 
