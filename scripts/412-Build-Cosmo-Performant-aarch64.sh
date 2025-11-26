dd6aa07c2f1ab42a5f326764013ffe9fdafb4e94
Brad Hutchings <brad@componentx.com>
2025-11-26 08:28:11 -0800

    Update 412-Build-Cosmo-Performant-aarch64.sh

--- scripts/412-Build-Cosmo-Performant-aarch64.sh
+++ scripts/412-Build-Cosmo-Performant-aarch64.sh
@@ -12,7 +12,7 @@ printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
 cd $BUILD_DIR
 
-AARCH64_ARCH_LEVEL_PARAM=" -march=armv8-a " 
+AARCH64_ARCH_LEVEL_PARAM=" $ARCH_AARCH64_PERFORMANT " 
 
 export PATH="$(pwd)/cosmocc/bin:$SAVE_PATH"
 
