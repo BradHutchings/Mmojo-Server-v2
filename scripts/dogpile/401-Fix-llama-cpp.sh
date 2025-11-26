ae23174fc69debf75c21b75e5908c95e6904d604
Brad Hutchings <brad@componentx.com>
2025-11-25 22:37:44 -0800

    Update 401-Fix-llama-cpp.sh

--- scripts/dogpile/401-Fix-llama-cpp.sh
+++ scripts/dogpile/401-Fix-llama-cpp.sh
@@ -30,7 +30,7 @@ sed -i -e 's/common.cpp/common-mmojo.cpp/g' common/CMakeLists.txt
 sed -i -e '/log.h/a \    mmojo-args.h\n\    mmojo-args.c' common/CMakeLists.txt
 sed -i -e 's/add_subdirectory(server)/add_subdirectory(server)\n\tif (COSMOCC)\n\t\tadd_subdirectory(zipalign)\n\tendif()/g' tools/CMakeLists.txt
 sed -i -e 's/server.cpp/server-mmojo.cpp/g' tools/server/CMakeLists.txt
-sed -i -e 's/server-hhtp.cpp/server-http-mmojo.cpp/g' tools/server/CMakeLists.txt
+sed -i -e 's/server-http.cpp/server-http-mmojo.cpp/g' tools/server/CMakeLists.txt
 sed -i -e 's/set(TARGET llama-server)/set(TARGET dogpile)/g' tools/server/CMakeLists.txt
 
 # What to do about this?!?
