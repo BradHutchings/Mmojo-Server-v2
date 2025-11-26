0a9ddf08b8c949062b97e7735bb06aa83731a228
Brad Hutchings <brad@componentx.com>
2025-11-26 07:44:51 -0800

    Create 303-Create-Certificates-Directory.sh

---
+++ scripts/303-Create-Certificates-Directory.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+################################################################################
+# This script creates the certificates directory.
+#
+# See licensing note at end.
+################################################################################
+
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
+mkdir -p $CERTIFICATES_DIR
+
+cd $HOME
+
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
