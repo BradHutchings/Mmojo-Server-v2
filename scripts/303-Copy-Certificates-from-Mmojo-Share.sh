1d2ef7f17ede84d985756aed8a7c1662a125a7d8
Brad Hutchings <brad@componentx.com>
2025-11-26 08:06:50 -0800

    Update 303-Copy-Certificates-from-Mmojo-Share.sh

--- scripts/303-Copy-Certificates-from-Mmojo-Share.sh
+++ scripts/303-Copy-Certificates-from-Mmojo-Share.sh
@@ -6,12 +6,15 @@
 # See licensing note at end.
 ################################################################################
 
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
 if [[ ! $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
     mm-mount-mmojo-share.sh
 fi
 
 if [[ $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
-    if [ -d $$MMOJO_SHARE_CERTIFICATES ]; then
+    if [ -d $MMOJO_SHARE_CERTIFICATES ]; then
         cp $MMOJO_SHARE_CERTIFICATES/cert.crt $CERTIFICATES_DIR
         cp $MMOJO_SHARE_CERTIFICATES/cert.key  $CERTIFICATES_DIR
         cp $MMOJO_SHARE_CERTIFICATES/selfsignCA.crt $CERTIFICATES_DIR
