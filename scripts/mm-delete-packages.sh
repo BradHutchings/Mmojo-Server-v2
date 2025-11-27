927c39f02e4beb14a98537d0689b5cf677b1d9b3
Brad Hutchings <brad@componentx.com>
2025-11-27 10:16:13 -0800

    Create mm-delete-packages.sh

---
+++ scripts/mm-delete-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+################################################################################
+# This script deletes the 500-PACKAGE-* directories.
+#
+# See licensing note at end.
+################################################################################
+
+export STARS="********************************************************************************"
+printf "\n$STARS\n*\n* STARTED: mm-environment-variables.sh.\n*\n$STARS\n\n"
+
+cd $HOME
+
+rm -r -f $PACKAGE_DIR
+rm -r -f $DOGPILE_PACKAGE_DIR
+
+printf "\n$STARS\n*\n* FINISHED: mm-environment-variables.sh.\n*\n$STARS\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
