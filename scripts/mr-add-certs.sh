9cee862e50e85931ad78614a249ea271107906c1
Brad Hutchings <brad@componentx.com>
2025-11-29 14:33:03 -0800

    Create mr-add-certs.sh

---
+++ scripts/mr-add-certs.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+################################################################################
+# This script creates a directory and files to start work on a new Mmojo Runner
+# archive.
+#
+# See licensing note at end.
+################################################################################
+
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME $1 $2.\n*\n$STARS\n\n"
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$1" == "" ]; then
+    echo "mr-add-certs.sh [RUNNER_DIR] [CERTS_PATH]"
+    echo "  Adds a directory of certificates or a certificate file to the runner."
+    exit 1
+fi
+
+runner_dir="$1"
+certs_path="$2"
+
+# Convert $runner_dir to an absolute path.
+case $runner_dir in
+  ("/"*) ;;
+  (*)    runner_dir="$(pwd)/$runner_dir";;
+esac
+
+# source $runner_dir/vars.sh
+support_subdirectory="support"
+vars_file="$runner_dir/vars.sh"
+if [ -f "$vars_file" ]; then
+    source $vars_file
+fi
+
+archive_zip="$runner_dir/archive.zip"
+support_dir="$runner_dir/$support_subdirectory"
+
+echo ""
+echo " \$runner_dir: $runner_dir"
+echo " \$certs_path: $certs_path"
+echo "\$archive_zip: $archive_zip"
+echo "\$support_dir: $support_dir"
+
+
+
+
+
+
+
+echo ""
+echo "Contents of $runner_dir/archive.zip:"
+unzip -l "$runner_dir/archive.zip"
+
+echo ""
+echo "Files in $runner_dir:"
+ls -alR "$runner_dir"
+
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME $1 $2.\n*\n$STARS\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
