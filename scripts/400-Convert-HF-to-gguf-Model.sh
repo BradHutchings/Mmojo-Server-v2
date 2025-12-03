ae80be949157bf6ef06a29753f760c2c44a33ffc
Brad Hutchings <brad@componentx.com>
2025-12-03 06:03:06 -0800

    Create 400-Convert-HF-to-gguf-Model.sh

---
+++ scripts/400-Convert-HF-to-gguf-Model.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+################################################################################
+# This script converts a Hugging Face repo with safetensor files into a .gguf 
+# model.
+#
+# https://github.com/ggml-org/llama.cpp
+#
+# See licensing note at end.
+################################################################################
+
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME $1 $2 $3 $4.\n*\n$STARS\n\n"
+
+model_name=$1
+model_type=$2
+model_mnemonic=$3
+model_repo=$4
+
+GGUF_DIR="$BUILD_MODELS_DIR/$model_name-$model_type"
+
+echo "    model_name: $model_name"
+echo "    model_type: $model_type"
+echo "model_mnemonic: $model_mnemonic"
+echo "    model_repo: $model_repo"
+echo "      GGUF_DIR: $GGUF_DIR"
+
+if [ "$model_name" != "" ] && [ "$model_type" != "" ] && [ "$model_mnemonic" != "" ] && [ "$model_repo" != "" ]; then
+    mkdir -p $BUILD_MODELS_DIR
+    if [ ! -d $GGUF_DIR ]; then
+        echo ""
+        echo "Cloning $model_repo."
+        git clone $model_repo $GGUF_DIR
+    fi
+
+    echo ""
+    echo "Converting to $model_name-$model_type.gguf."
+    python3 $BUILD_DIR/convert_hf_to_gguf.py $GGUF_DIR \
+        --outfile $GGUF_DIR/$model_name-$model_type.gguf \
+        --outtype $model_type
+
+    if [ -d "$MODELS_DIR" ]; then
+        echo ""
+        echo "Copying to $MODELS_DIR."
+        rsync -ah --progress $GGUF_DIR/$MODEL_NAME-$MODEL_TYPE.gguf $MODELS_DIR
+    fi
+fi
+
+cd $HOME
+
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME $1 $2 $3 $4.\n*\n$STARS\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
