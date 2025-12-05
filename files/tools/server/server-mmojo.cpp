c9763f6fa48ba0c32502a1a6f088c39a21913e47
Brad Hutchings <brad@componentx.com>
2025-12-04 22:10:24 -0800

    Update server-mmojo.cpp

--- files/tools/server/server-mmojo.cpp
+++ files/tools/server/server-mmojo.cpp
@@ -200,6 +200,10 @@ int main(int argc, char ** argv, char ** envp) {
     char pathChar[PATH_MAX];
     pathChar[0] = '\0';
 
+    printf("argv[0]: %s\n", argv[0]);
+    printf("argv[1]: %s\n", argv[1]);
+    printf("argv[2]: %s\n", argv[2]);
+
     #ifdef COSMOCC
         printf("Getting working directory path for Cosmo build.\n");
 
