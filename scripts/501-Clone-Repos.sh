5095594ce403cd9cc6fcefc83cee691e6a15e4f6
Brad Hutchings <brad@componentx.com>
2025-12-05 21:51:03 -0800

    Update 501-Clone-Repos.sh

--- scripts/501-Clone-Repos.sh
+++ scripts/501-Clone-Repos.sh
@@ -57,7 +57,8 @@ cp -r $BUILD_COSMOPOLITAN_DIR/cosmocc .
 cp -r $BUILD_OPENSSSL_DIR/openssl .
 
 cd cpp-httplib
-git checkout 1d36013fc3aa14f39f5e8071be445e5a75fafff7
+# git checkout 1d36013fc3aa14f39f5e8071be445e5a75fafff7 # worked!
+git checkout dbd5ca4bf2da438da1c6dcd0ba7c725f7f2cc7b7
 cd ..
 
 # Roll the llama.cpp repo back to last known working.
