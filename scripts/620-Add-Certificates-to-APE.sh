489c87fb9e615c384af4b107600532b61ff08bcc
Brad Hutchings <brad@componentx.com>
2025-12-13 16:44:30 -0800

    Update 620-Add-Certificates-to-APE.sh

--- scripts/620-Add-Certificates-to-APE.sh
+++ scripts/620-Add-Certificates-to-APE.sh
@@ -21,7 +21,7 @@ if [ "$attire" != "attired" ] && [ "$attire" != "naked" ]; then
     attire="attired"
 fi
 
-if [ "$branding" != "doghouse" ]; then
+if [ "$branding" != "doghouse" ] && [ "$branding" != "llama-server" ]; then
     branding=""
 fi
 
@@ -41,6 +41,9 @@ ZIP_FILE="$PACKAGE_MMOJO_SERVER_ZIP_FILE"
 if [ "$branding" == "doghouse" ]; then
     THIS_PACKAGE_DIR="$DOGHOUSE_PACKAGE_DIR/$PACKAGE_SUBDIRECTORY"
     ZIP_FILE="$PACKAGE_DOGHOUSE_ZIP_FILE"
+elif [ "$branding" == "llama-server" ]; then
+    THIS_PACKAGE_DIR=$LLAMA_SERVER_PACKAGE_DIR
+    ZIP_FILE="$PACKAGE_LLAMA_SERVER_ZIP_FILE"
 fi
 
 if [ -v CHOSEN_MODEL_MNEMONIC ]; then
