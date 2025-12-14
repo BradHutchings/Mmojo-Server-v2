1954b8e69c2c405868263584f8ecf579b60ebfb2
Brad Hutchings <brad@componentx.com>
2025-12-13 16:47:18 -0800

    Update 620-Add-gguf-Model-to-APE.sh

--- scripts/620-Add-gguf-Model-to-APE.sh
+++ scripts/620-Add-gguf-Model-to-APE.sh
@@ -21,8 +21,18 @@ if [ "$attire" != "attired" ] && [ "$attire" != "naked" ]; then
     attire="attired"
 fi
 
-if [ "$branding" != "doghouse" ]; then
+if [ "$branding" != "doghouse" ] && [ "$branding" != "llama-server" ]; then
     branding=""
+fi
+
+THIS_PACKAGE_DIR="$PACKAGE_DIR/$PACKAGE_SUBDIRECTORY"
+ZIP_FILE="$PACKAGE_MMOJO_SERVER_ZIP_FILE"
+if [ "$branding" == "doghouse" ]; then
+    THIS_PACKAGE_DIR="$DOGHOUSE_PACKAGE_DIR/$PACKAGE_SUBDIRECTORY"
+    ZIP_FILE="$PACKAGE_DOGHOUSE_ZIP_FILE"
+elif [ "$branding" == "llama-server" ]; then
+    THIS_PACKAGE_DIR=$LLAMA_SERVER_PACKAGE_DIR
+    ZIP_FILE=$PACKAGE_LLAMA_SERVER_ZIP_FILE
 fi
 
 PACKAGE_SUBDIRECTORY="$PACKAGE_COMPATIBLE_APE"
@@ -36,15 +46,8 @@ if [ "$variation" == "performant" ]; then
     fi
 fi
 
-THIS_PACKAGE_DIR="$PACKAGE_DIR/$PACKAGE_SUBDIRECTORY"
-ZIP_FILE="$PACKAGE_MMOJO_SERVER_ZIP_FILE"
-if [ "$branding" == "doghouse" ]; then
-    THIS_PACKAGE_DIR="$DOGHOUSE_PACKAGE_DIR/$PACKAGE_SUBDIRECTORY"
-    ZIP_FILE="$PACKAGE_DOGHOUSE_ZIP_FILE"
-fi
-
-if [ -v CHOSEN_MODEL_MNEMONIC ]; then
-    THIS_PACKAGE_DIR+="-$CHOSEN_MODEL_MNEMONIC"
+if [ "$attire" == "attired" ] && [ -v CHOSEN_MODEL_MNEMONIC ]; then
+    PACKAGE_SUBDIRECTORY+="-$CHOSEN_MODEL_MNEMONIC"
 fi
 
 echo "             Variation: $variation"
