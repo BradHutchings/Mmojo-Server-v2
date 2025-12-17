37b56fe6ce7e46444f2f8c6df93e058a41aa9682
Brad Hutchings <brad@componentx.com>
2025-12-17 13:49:38 -0800

    Update 401-Download-Convert_Script.sh

--- scripts/401-Download-Convert_Script.sh
+++ scripts/401-Download-Convert_Script.sh
@@ -14,12 +14,20 @@
 SCRIPT_NAME=$(basename -- "$0")
 printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
-mkdir -p $LOCAL_MODELS_DIR
-url="https://raw.githubusercontent.com/ggml-org/llama.cpp/refs/heads/master/convert_hf_to_gguf.py"
-filename="convert_hf_to_gguf.py"
+echo "RENAME THIS SCRIPT!!"
 
-echo "Downloading $filename."
-wget $url -O "$LOCAL_MODELS_DIR/$filename"
+mkdir -p $LOCAL_MODELS_DIR
+cd $LOCAL_MODELS_DIR
+
+echo "Cloning llama.cpp locally for gguf python stack."
+git clone https://github.com/ggml-org/llama.cpp $LOCAL_MODELS_DIR/llama.cpp
+
+
+# url="https://raw.githubusercontent.com/ggml-org/llama.cpp/refs/heads/master/convert_hf_to_gguf.py"
+# filename="convert_hf_to_gguf.py"
+
+# echo "Downloading $filename."
+# wget $url -O "$LOCAL_MODELS_DIR/$filename"
 
 cd $HOME
 
