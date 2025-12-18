c43fdc039ef2a355ad09d63e47f4bb9cebd51ad0
Brad Hutchings <brad@componentx.com>
2025-12-18 09:35:31 -0800

    Create 401-Download-Model-Map.sh

---
+++ scripts/401-Download-Model-Map.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+################################################################################
+# This script downloads a model map file from Hugging Face. The model map file
+# identifies models we might want to download, and suggest names for their
+# self-contaned APE files we will package later.
+#
+# See licensing note at end.
+################################################################################
+
+SCRIPT_NAME=$(basename -- "$0")
+printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
+MODEL_MAP_URL="https://huggingface.co/bradhutchings/Mmojo-Server/resolve/main/model-map.txt"
+wget $MODEL_MAP_URL --quiet -O $LOCAL_MODEL_MAP
+
+printf "\n$STARS\n*\n* FINISHED: $SCRIPT_NAME.\n*\n$STARS\n\n"
+
+################################################################################
+#  This is an original script for the Mmojo Server repo. It is covered by
+#  the repo's MIT-style LICENSE:
+#
+#  https://github.com/BradHutchings/Mmojo-Server/blob/main/LICENSE
+#
+#  Copyright (c) 2025 Brad Hutchings.
+#  --
+#  Brad Hutchings
+#  brad@bradhutchings.com
+################################################################################
