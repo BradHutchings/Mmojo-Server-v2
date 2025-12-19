943234f32c8c76e54060012d0a2c9d72ea26e8bf
Brad Hutchings <brad@componentx.com>
2025-12-19 11:35:10 -0800

    Update 303-Copy-Certificates-from-Mmojo-Share.sh

--- scripts/303-Copy-Certificates-from-Mmojo-Share.sh
+++ scripts/303-Copy-Certificates-from-Mmojo-Share.sh
@@ -9,6 +9,11 @@
 SCRIPT_NAME=$(basename -- "$0")
 printf "\n$STARS\n*\n* STARTED: $SCRIPT_NAME.\n*\n$STARS\n\n"
 
+if [ ! -d $MMOJO_SHARE_MOUNT_POINT ]; then
+    echo "You have not created your Mmojo Share mount point."
+    exit 1
+fi
+
 if [[ ! $(findmnt $MMOJO_SHARE_MOUNT_POINT) ]]; then
     mm-mount-mmojo-share.sh
 fi
